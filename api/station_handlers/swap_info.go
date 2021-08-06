3f0820963ca956e72e89ecacb751d14776ef7c91
tpkeeper <tpkeeper.me@gmail.com>
2021-08-06 18:10:00 +0800

    add param check

--- api/station_handlers/swap_info.go
+++ api/station_handlers/swap_info.go
@@ -5,21 +5,23 @@ import (
 	"fee-station/pkg/utils"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 type ReqSwapInfo struct {
-	StafiAddress string `json:"stafiAddress"`
+	StafiAddress string `json:"stafiAddress"` //hex
 	Symbol       string `json:"symbol"`
-	Blockhash    string `json:"blockHash"`
-	Txhash       string `json:"txHash"`
+	Blockhash    string `json:"blockHash"` //hex
+	Txhash       string `json:"txHash"`    //hex
 	PoolAddress  string `json:"poolAddress"`
-	Signature    string `json:"signature"`
-	Pubkey       string `json:"pubkey"`
-	InAmount     string `json:"inAmount"`
-	MinOutAmount string `json:"minOutAmount"`
+	Signature    string `json:"signature"`    //hex
+	Pubkey       string `json:"pubkey"`       //hex
+	InAmount     string `json:"inAmount"`     //decimal
+	MinOutAmount string `json:"minOutAmount"` //decimal
 }
 
 // @Summary post swap info
@@ -43,8 +45,46 @@ func (h *Handler) HandlePostSwapInfo(c *gin.Context) {
 		utils.Err(c, "symbol unsupport")
 		return
 	}
+	//check 0x prefix param
+	var stafiAddressBytes []byte
+	if stafiAddressBytes, err = hexutil.Decode(req.StafiAddress); err != nil {
+		utils.Err(c, "stafiAddress format err")
+		return
+	}
+	if _, err := hexutil.Decode(req.Blockhash); err != nil {
+		utils.Err(c, "blockHash format err")
+		return
+	}
+	if _, err := hexutil.Decode(req.Txhash); err != nil {
+		utils.Err(c, "txHash format err")
+		return
+	}
 
-	//check duplicate
+	var sigBytes []byte
+	var pubkeyBytes []byte
+	if sigBytes, err = hexutil.Decode(req.Signature); req.Symbol != utils.SymbolAtom && err != nil {
+		utils.Err(c, "signature format err")
+		return
+	}
+	if pubkeyBytes, err = hexutil.Decode(req.Pubkey); req.Symbol != utils.SymbolAtom && err != nil {
+		utils.Err(c, "pubkey format err")
+		return
+	}
+
+	//check pool address
+	poolAddr, err := dao_station.GetPoolAddressBySymbol(h.db, req.Symbol)
+	if err != nil {
+		utils.Err(c, "get pool address failed")
+		logrus.Errorf("dao_station.GetPoolAddressBySymbol err %v", err)
+		return
+	}
+	if !strings.EqualFold(poolAddr.PoolAddress, req.PoolAddress) {
+		utils.Err(c, "pool address not right")
+		logrus.Errorf("pool address not right:req %s,db:%s", req.PoolAddress, poolAddr.PoolAddress)
+		return
+	}
+
+	//check block hash and tx hash duplicate
 	swapInfo, err := dao_station.GetSwapInfoBySymbolBlkTx(
 		h.db, req.Symbol, strings.ToLower(req.Blockhash), strings.ToLower(req.Txhash))
 	if err != nil && err != gorm.ErrRecordNotFound {
@@ -56,6 +96,24 @@ func (h *Handler) HandlePostSwapInfo(c *gin.Context) {
 		utils.Err(c, "duplicate swap info")
 		logrus.Errorf("duplicate swap info, txhash:", req.Txhash)
 		return
+	}
+
+	//check signature
+	switch req.Symbol {
+	case utils.SymbolDot, utils.SymbolKsm:
+		ok := utils.VerifySigsSecp256(sigBytes, pubkeyBytes, stafiAddressBytes)
+		if !ok {
+			utils.Err(c, "signature not right")
+			logrus.Errorf("utils.VerifySigsSecp256 failed, stafi address: %s", req.StafiAddress)
+			return
+		}
+	case utils.SymbolEth:
+		ok := utils.VerifySigsEth(sigBytes, stafiAddressBytes, common.BytesToAddress(pubkeyBytes))
+		if !ok {
+			utils.Err(c, "signature not right")
+			logrus.Errorf("utils.VerifySigsEth failed, stafi address: %s", req.StafiAddress)
+			return
+		}
 	}
 
 	swapInfo.StafiAddress = req.StafiAddress
