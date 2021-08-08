6cea076a7778098a9dde13817b797f71e2b4e9ad
tpkeeper <tpkeeper.me@gmail.com>
2021-08-08 23:19:58 +0800

    fix field len small err

--- dao/station/swap_info.go
+++ dao/station/swap_info.go
@@ -5,14 +5,14 @@ import "fee-station/pkg/db"
 // swap info
 type SwapInfo struct {
 	db.BaseModel
-	StafiAddress  string `gorm:"type:varchar(42);not null;default:'';column:stafi_address"` //base58 address
+	StafiAddress  string `gorm:"type:varchar(80);not null;default:'0x';column:stafi_address"` //base58 address
 	State         uint8  `gorm:"type:tinyint(1);unsigned;not null;default:0;column:state"`  //0 verify sigs 1 verify tx ok 2 verify tx failed 3 swap ok
 	Symbol        string `gorm:"type:varchar(10);not null;default:'symbol';column:symbol"`
 	Blockhash     string `gorm:"type:varchar(80);not null;default:'0x';column:block_hash;uniqueIndex:uni_idx_blk_tx"`
 	Txhash        string `gorm:"type:varchar(80);not null;default:'0x';column:tx_hash;uniqueIndex:uni_idx_blk_tx"`
 	PoolAddress   string `gorm:"type:varchar(80);not null;default:'';column:pool_address"`
-	Signature     string `gorm:"type:varchar(100);not null;default:'0x';column:signature"`
-	Pubkey        string `gorm:"type:varchar(70);not null;default:'0x';column:pubkey"` // when symbol is eth ,this is user address
+	Signature     string `gorm:"type:varchar(150);not null;default:'0x';column:signature"`
+	Pubkey        string `gorm:"type:varchar(80);not null;default:'0x';column:pubkey"` // when symbol is eth ,this is user address
 	InAmount      string `gorm:"type:varchar(30);not null;default:'0';column:in_amount"`
 	MinOutAmount  string `gorm:"type:varchar(30);not null;default:'0';column:min_out_amount"`
 	OutAmount     string `gorm:"type:varchar(30);not null;default:'0';column:out_amount"`
